11365208b0fc098bf33ae3094f9e8dec9ae65ceb
[Scott Watson] <scottwat@gmail.com>
2019-08-07 23:30:58 -0700

    08-07-2019

--- ch1.py
+++ ch1.py
@@ -2,6 +2,7 @@
 #               https://d.cxcore.net/Python/Python_Cookbook_3rd_Edition.pdf
 #               Chapter 1 - Data Structures and Algorithms
 
+###############################################
 #                1.1 Unpacking a SEQUENCE / LIST / ITERABLE into a separate variable
 
 """
@@ -25,6 +26,8 @@ Date: {mon}-{day}-{year}
 Price: $%.2f
 """ % price)
 
+
+###############################################
 # 1.2 Unpacking Elements from Iterables of Arbitrary Length
 """
 Problem: Unpacks ITERABLES with unknown length
@@ -61,7 +64,7 @@ print(f"""
 {sh}            /usr/bin/false
 """)
 
-
+###############################################
 #               1.3 Keeping the Last N Items
 """
 You want yo keep a limited history of the last few items seen during iterations or during some
@@ -94,7 +97,8 @@ if  __name__ == '__main__':
                 print(pline, end="")
                 print(line, end="")
                 print('-'*20)
-                
+
+###############################################
 # 1.4 Finding the Large or Smallest N Items
 # priority queue algorithm
 print('\n*********')
@@ -119,35 +123,182 @@ expensive = heapq.nlargest(1, portfolio, key=lambda s: s['price'])
 print(f'Expensive Stock: {expensive}')
 
 
-# 1.5 Priority Queue
+###############################################
+# 1.5 Priority Queue that sorts by priority and always returns highest priority
 
 import heapq
 class PriorityQueue:
+    """
+    Class implements a priority queue of a list using heapq
+
+    # EXAMPLE
+        class Item:
+            def __init__(self, name):
+                self.name = name
+            def __repr__(self):
+                return 'Item({!r})'.format(self.name)
+
+        INSTANTIATE PRIORITYQUEUE OBJECT
+        q = PriorityQueue() #
+
+        ASSEMBLING PRIORITY QUEUE ELEMENT
+        q.push(Item('foo'), 1)
+        q.push(Item('bar'), 5)
+        q.push(Item('spam'), 4)
+        q.push(Item('grok'), 1)
+
+        POPPING HIGHEST PRIORITY FIRST
+        print(q.pop())      # Item('bar')
+        print(q.pop())      #Item('spam')
+        print(q.pop())      # Item('foo')
+        print(q.pop())      # Item('grok')   -> grok was added last and was popped last
+
+    """
+
   def __init__(self):
-    self._queue = []
-    self._index = 0
-  
+      """ Init Method intialized local variables """
+      self._queue = []
+      self._index = 0  # Differentiates between objects with same priority
+
   def push(self, item, priority):
-    heapq.heappush(self._queue, (-priority, self._index, item))
-    self._index += 1
-  
+     """ Push Method takes self, item, priority as input and adds to sorted queue"""
+     # (-)priority to reverse with highest priority first
+     heapq.heappush(self._queue, (-priority, self._index, item))
+     self._index += 1
+
   def pop(self):
-    return heapq.heappop(self._queue)[-1]
+     """ Pop Method takes in self as input and returns the highest priority item in the queue """
+     return heapq.heappop(self._queue)[-1]
 
-class Item:
-  def __init__(self, name):
-    self.name = name
-  def __repr__(self):
-    return 'Item({!r})'.format(self.name)
+###############################################
+#    1.6 Mapping Keys to Multiple Values in a Dictoionary aka MULTIDICTIONARY
+
+# Simple Method
+# Use [list] if you want to preserve the insertion order
+# Use {set} to eliminate duplicates and order isn't important
+d = {
+                'a': [1, 2, 3],
+                'b': [4, 5]
+      }
+
+e = {
+        'a': {1, 2, 3},
+        'b': {4, 5}
+       }
+
+# More Sophisticated Method
+# CAUTION - defaultdict creates items that don't exist if access is attempted
+# defaultdict makes it easier to initialize the first value as you don't need to verify that it exists
+from collections import defaultdict
+
+pairs = [('yellow', 1), ('yellow', 3), ('blue', 4), ('red', 1), ('blue', 2)]
+
+# list argument passed indicates the object
+d = defaultdict(list)
+for key, value in pairs:
+    d[key].append(value)
+
+d['orange'].append(9)
+d.items()  #dict_items([('yellow', [1, 3]), ('blue', [4, 2]), ('red', [1]), ('orange', [9])])
+d['yellow'][1] #3
+
+# set argument
+s = defaultdict(set)
+for key, value in pairs:
+    s[key].add(value)
+
+s.items()  #dict_items([('yellow', {1, 3}), ('blue', {2, 4}), ('red', {1})])
+s['yellow'].pop() #1
 
 
-q = PriorityQueue()
-q.push(Item('foo'), 1)
-q.push(Item('bar'), 5)
-q.push(Item('spam'), 4)
-q.push(Item('grok'), 1)
+###############################################
+#    1.7 Keeping Dictionairiers in Order
+# OrderedDict are useful for mapping/serialize/encoding dict into a different format -> JSON / YAML
+# OderedDict are implemented with double link lists
+# as a result they are more than TWICE THE SIZE of regular dictionaries
+# -> MEMORY TRADE OFF with large data sets
+from collections import OrderedDict
 
-print(q.pop())
-print(q.pop())
-print(q.pop())
-print(q.pop())
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+d['spam'] = 3
+d['grok'] = 4
+
+# OUTPUT    "foo 1", "bar 2", "spam 3", "grok 4"
+for key in d:
+    print(key, d[key])
+
+#  1.8 Calculating with Dictionary Values
+# NOTE zip() creates an iterator than can only be consumed once
+prices =   {
+                    'ACME': 45.25,
+                    'AAPL': 612.78,
+                    'IBM': 205.55,
+                    'HPQ': 37.20,
+                    'FB': 10.75
+                }
+
+min_price = min(zip(prices.values(), prices.keys()))  # swaps values for keys to find min price
+min_price  # = (10.75, 'FB')
+max_price = max(zip(prices.values(), prices.keys())) # swaps values for keys to find max price
+max_price # = (612.78, 'AAPL')
+
+prices_sorted = sorted(zip(prices.values(), prices.keys()))
+prices_sorted # [(10.75, 'FB'), (37.2, 'HPQ'), (45.25, 'ACME'), (205.55, 'IBM'), (612.78, 'AAPL')]
+
+# KEY ONLY
+min(prices, key=lambda k: prices[k])  #'FB'
+# VALUE ONLY
+min_value = prices[min(prices, key=lambda k: prices[k])] # 10.75
+
+
+###############################################
+# 1.9 Finding Commonalities in Two Dictionaries
+
+a = { 'x': 1, 'y': 2, 'z': 3 }
+
+b =  {'w': 10, 'x': 11, 'y': 2}
+
+# Find keys in common
+a.keys() & b.keys() # {'x', 'y'}
+
+# & bitwise comparison
+
+# Find keys in a that are not in b
+a.keys() - b.keys() # {'z'}
+
+# Find (key, value) pairs in common
+a.items() & b.items() # {('y', 2)}
+
+# Filtering dictionary contents
+c = {key:a[key] for key in a.keys() - {'z', 'w'}} # {'x': 1, 'y': 2}
+
+# keys() method return keys-views which support common SET operations eg union, intersection,
+# items() method returns items-view (key, value) pairs; which also support similar SET operations
+# values() DOES NOT SUPPORT set operations
+
+
+###############################################
+# 1.10 Removing Duplicates from a Sequence while Maintaining Order
+
+def dedupe(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item) # LISTS -> remove this line
+        if val not in seen: #  LISTS -> val is replaced with item
+            yield item
+            seen.add(val) LISTS -> val is replaced with item
+
+# a = [1, 5, 2, 1, 9, 1, 5, 10]
+# list(dedup(a)) #[1, 5, 2, 9, 10]
+
+a = [{'x': 1, 'y':2}, {'x':1, 'y':3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+list(dedupe(a, key=lambda d: (d['x']))) # [{'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+
+# Converting LIST to SET accomblished dedup but at the cost of losing the Order
+
+# # By using a generator dedupe() can be used to remove duplicate lines
+# with open('test.txt', 'r+') as f:
+#     for line in depupe(f):
+#         print(line)
